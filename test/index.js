a85a6d0a69e3cf93e2f810a28ed617ba971a1a47
davidmarkclements <huperekchuno@googlemail.com>
2017-04-28 23:57:43 +0100

    lint

--- test/index.js
+++ test/index.js
@@ -124,9 +124,10 @@ test(':date[iso]', function (t) {
 })
 
 test(':date[clf]', function (t) {
+  /* eslint no-extend-native: 0 */
   var getTimezoneOffset = Date.prototype.getTimezoneOffset
   Date.prototype.getTimezoneOffset = function () { return 0 }
-  var expected = '21/Jul/2016:17:34:52 -0000\n'
+  var expected = '21/Jul/2016:17:34:52 +0000\n'
   var logger = format(':date[clf]', through(function (line) {
     t.is(line.toString(), expected)
     Date.prototype.getTimezoneOffset = getTimezoneOffset
