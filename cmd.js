1d3547d576d745b0c01c6ecd859dee981dc37fdf
Matteo Collina <hello@matteocollina.com>
2020-04-15 12:03:18 +0200

    Updated dependencies, remove deprecated stdio logic

--- cmd.js
+++ cmd.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
+
 var toke = require('./')
-var stdio = require('./stdio')
-var args = require('minimist')(process.argv.slice(2), { alias: {
-  h: ['help'],
-  a: ['ancillary'],
-  d: ['dest', 'destination'],
-  k: ['keep']
-} })
+var SonicBoom = require('sonic-boom')
+var args = require('minimist')(process.argv.slice(2), {
+  alias: {
+    h: ['help'],
+    a: ['ancillary'],
+    d: ['dest', 'destination'],
+    k: ['keep']
+  }
+})
 
 if (args.h) {
   console.log(require('fs').readFileSync(require('path').join(__dirname, 'usage.txt')) + '')
@@ -24,12 +27,14 @@ else if ((args.a + '').toLowerCase() === 'stderr') args.a = 2
 
 if (args.d === 1) destination = process.stdout
 else if (args.d === 2) destination = process.stderr
-else if (args.d !== undefined) destination = stdio(args.d)
+else if (args.d !== undefined) destination = SonicBoom({ dest: parseInt(args.d) })
 
 if (args.a === 1) ancillary = process.stdout
 else if (args.a === 2) ancillary = process.stderr
-else if (args.a !== undefined) ancillary = stdio(args.a)
+else if (args.a !== undefined) ancillary = SonicBoom({ dest: parseInt(args.a) })
 
-if (args.k) format = { format: format, keep: true }
+if (args.k) {
+  format = { format: format, keep: true }
+}
 
 process.stdin.pipe(toke(format, destination, ancillary))
--- test/cmd.js
+++ test/cmd.js
@@ -37,7 +37,7 @@ test('--help', function (t) {
 })
 
 test('tokens', function (t) {
-  var expected = 'MacBook-Pro-4 13961 GET /api/activity/component 200 (17 ms) application/json; charset=utf-8 _ga=GA1.1.204420087.1444842476\n'
+  var expected = 'MacBook-Pro-4 13961 GET /api/activity/component 200 (17 ms) application/json; charset=utf-8 _ga=GA1.1.204420087.1444842476\n\n'
   var args = [':hostname', ':pid', ':method', ':url', ':status', '(:response-time', 'ms)', ':res[content-type]', ':req[cookie]']
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
     cwd: cwd,
@@ -51,7 +51,7 @@ test('tokens', function (t) {
 })
 
 test('-d', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-d', '2', ':pid']
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
     cwd: cwd,
@@ -65,7 +65,7 @@ test('-d', function (t) {
 })
 
 test('--destination', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['--destination', '2', ':pid']
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
     cwd: cwd,
@@ -79,7 +79,7 @@ test('--destination', function (t) {
 })
 
 test('--dest', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['--dest', '2', ':pid']
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
     cwd: cwd,
@@ -107,7 +107,7 @@ test('-d with custom fd', function (t) {
 })
 
 test('-d 1', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-d', '1', ':pid']
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
     cwd: cwd,
@@ -121,7 +121,7 @@ test('-d 1', function (t) {
 })
 
 test('-d stderr', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-d', 'stderr', ':pid']
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
     cwd: cwd,
@@ -135,7 +135,7 @@ test('-d stderr', function (t) {
 })
 
 test('-d stdout', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-d', 'stdout', ':pid']
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
     cwd: cwd,
@@ -149,7 +149,7 @@ test('-d stdout', function (t) {
 })
 
 test('-a', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-a', '2', ':pid']
   var msg = '{"pid":94473,"hostname":"MacBook-Pro-3.home","level":30,"msg":"hello world","time":1459529098958,"v":1}\n'
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
@@ -165,7 +165,7 @@ test('-a', function (t) {
 })
 
 test('--ancillary', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['--ancillary', '2', ':pid']
   var msg = '{"pid":94473,"hostname":"MacBook-Pro-3.home","level":30,"msg":"hello world","time":1459529098958,"v":1}\n'
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
@@ -181,7 +181,7 @@ test('--ancillary', function (t) {
 })
 
 test('-a with custom fd', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-a', '3', ':pid']
   var msg = '{"pid":94473,"hostname":"MacBook-Pro-3.home","level":30,"msg":"hello world","time":1459529098958,"v":1}\n'
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
@@ -197,7 +197,7 @@ test('-a with custom fd', function (t) {
 })
 
 test('-a 1 -d 2', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-a', '1', '-d', '2', ':pid']
   var msg = '{"pid":94473,"hostname":"MacBook-Pro-3.home","level":30,"msg":"hello world","time":1459529098958,"v":1}\n'
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
@@ -213,7 +213,7 @@ test('-a 1 -d 2', function (t) {
 })
 
 test('-a stderr', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-a', 'stderr', ':pid']
   var msg = '{"pid":94473,"hostname":"MacBook-Pro-3.home","level":30,"msg":"hello world","time":1459529098958,"v":1}\n'
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
@@ -229,7 +229,7 @@ test('-a stderr', function (t) {
 })
 
 test('-a stdout -d 2', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-a', 'stdout', '-d', '2', ':pid']
   var msg = '{"pid":94473,"hostname":"MacBook-Pro-3.home","level":30,"msg":"hello world","time":1459529098958,"v":1}\n'
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
@@ -245,7 +245,7 @@ test('-a stdout -d 2', function (t) {
 })
 
 test('-a 1 -d 1', function (t) {
-  var expected = '13961\n'
+  var expected = '13961'
   var args = ['-a', '1', '-d', '1', ':pid']
   var msg = '{"pid":94473,"hostname":"MacBook-Pro-3.home","level":30,"msg":"hello world","time":1459529098958,"v":1}\n'
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
@@ -253,13 +253,13 @@ test('-a 1 -d 1', function (t) {
     stdio: ['pipe', 'pipe', 'pipe'],
     input: log + msg
   })
-  t.is(result.output[1] + '', expected + msg)
+  t.is(result.output[1] + '', expected + '\n' + msg + '\n')
 
   t.end()
 })
 
 test('-k -a 2', function (t) {
-  var expected = '13961\n'
+  var expected = '13961\n\n'
   var args = ['-k', '-a', '2', ':pid']
   var msg = '{"pid":94473,"hostname":"MacBook-Pro-3.home","level":30,"msg":"hello world","time":1459529098958,"v":1}\n'
   var result = cp.spawnSync('node', ['cmd.js'].concat(args), {
@@ -271,88 +271,5 @@ test('-k -a 2', function (t) {
   t.is(result.output[1] + '', expected)
   t.is(result.output[2] + '', log + msg)
 
-  t.end()
-})
-
-// test('redirect custom fd to file (& unexpected close)', function (t) {
-//   var expected = '13961\n'
-//   var tmp = path.join(__dirname, 'tmp')
-//   var out = fs.openSync(tmp, 'w')
-//   var args = ['-d', '3', ':pid']
-//   cp.spawnSync('node', ['cmd.js'].concat(args), {
-//     cwd: cwd,
-//     stdio: ['pipe', 'pipe', 'pipe', out],
-//     input: log
-//   })
-
-//   setTimeout(() => {
-//     t.is(fs.readFileSync(tmp).toString().trim(), expected.trim())
-//     fs.unlinkSync(tmp)
-//     t.end()
-//   }, 1000)
-// })
-
-test('generate TTY case', function (t) {
-  var tmp = path.join(__dirname, 'tmp')
-  fs.writeFileSync(tmp, `
-    var binding = process.binding
-    process.binding = function (ns) {
-      if (ns === 'tty_wrap') {
-        var b = binding(ns)
-        var guessHandleType = b.guessHandleType
-        b.guessHandleType = function (fd) {
-          // calls to this method differs in coverage, to straight test run
-          // so have to determine when to mock to avoid throwing in the wrong place
-          var mock = Error().stack.toString().match('${path.join(cwd, 'stdio.js')}')
-          return mock ? 'TTY' : guessHandleType(fd)
-        }
-        return b
-      }
-      return binding(ns)
-    }
-    require('../cmd.js')
-  `)
-  var expected = '13961\n'
-  var args = ['-d', '3', ':pid']
-  var result = cp.spawnSync('node', [tmp].concat(args), {
-    cwd: cwd,
-    stdio: ['pipe', 'pipe', 'pipe', 'pipe'],
-    input: log
-  })
-
-  t.is(result.output[3] + '', expected)
-  fs.unlinkSync(tmp)
-  t.end()
-})
-
-test('generate unsupported stdio', function (t) {
-  var tmp = path.join(__dirname, 'tmp')
-  fs.writeFileSync(tmp, `
-    var binding = process.binding
-    process.binding = function (ns) {
-      if (ns === 'tty_wrap') {
-        var b = binding(ns)
-        var guessHandleType = b.guessHandleType
-        b.guessHandleType = function (fd) {
-          // calls to this method differs in coverage, to straight test run
-          // so have to determine when to mock to avoid throwing in the wrong place
-          var mock = Error().stack.toString().match('${path.join(cwd, 'stdio.js')}')
-          return mock ? 'UNKNOWN' : guessHandleType(fd)
-        }
-        return b
-      }
-      return binding(ns)
-    }
-    require('../cmd.js')
-  `)
-  var args = ['-d', '3', ':pid']
-  var result = cp.spawnSync('node', [tmp].concat(args), {
-    cwd: cwd,
-    stdio: ['pipe', 'pipe', 'pipe'],
-    input: log
-  })
-
-  t.ok(/Error: Unknown stream type/.test(result.stderr + ''))
-  fs.unlinkSync(tmp)
   t.end()
 })
